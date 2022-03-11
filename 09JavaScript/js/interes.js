fbe9b314c84edb17cb9c06d69e2dc2ecb3d7710d
CinthyaJG9 <cjimenezg2000@alumno.ipn.mx>
2022-03-11 17:26:21 -0600

    interes js

---
+++ 09JavaScript/js/interes.js
@@ -0,0 +1,12 @@
+/*
+funcion por medio de exp. regular*/
+
+function validarn(e){
+
+    var teclado=(document.all)? e.keyCode: e.which;
+    if(teclado==8) return true;
+    var patron=/[0-9\d .]/;
+    var codigo= String.fromCharCode(teclado);
+    return patron.test(codigo)
+    
+}
