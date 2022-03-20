35567c09bffba40ac4cc1999229bd8ba5eb14ab3
CinthyaJG9 <cjimenezg2000@alumno.ipn.mx>
2022-03-19 23:46:16 -0600

    problema 2

--- 10AlgoritmiaJS/js/logica.js
+++ 10AlgoritmiaJS/js/logica.js
@@ -1,5 +1,46 @@
-//problema 3
+//problema2
 
+function problema2(){
+
+    var p2_x1 = document.querySelector('#p2-x1').value;
+    var p2_x2 = document.querySelector('#p2-x2').value;
+    var p2_x3 = document.querySelector('#p2-x3').value;
+    var p2_x4 = document.querySelector('#p2-x4').value;
+    var p2_x5 = document.querySelector('#p2-x5').value;
+
+    var p2_y1 = document.querySelector('#p2-y1').value;
+    var p2_y2 = document.querySelector('#p2-y2').value;
+    var p2_y3 = document.querySelector('#p2-y3').value;
+    var p2_y4 = document.querySelector('#p2-y4').value;
+    var p2_y5 = document.querySelector('#p2-y5').value;
+
+
+    var vector1=[p2_x1, p2_x2, p2_x3, p2_x4, p2_x5];
+    var vector2=[p2_y1, p2_y2, p2_y3, p2_y4, p2_y5];
+
+    vector1=vector1.sort(function(x, y){
+
+        return y-x;
+    });
+
+    vector2=vector2.sort(function(x, y){
+
+        return y-x;
+    });
+
+    vector2=vector2.reverse();
+
+    var p2_resultado=0;
+
+    for(var i=0; i<vector1.length; i++){
+
+        p2_resultado+= vector1[i]*vector2[i];
+    }
+
+    document.querySelector('#p2-output').textContent="El producto escalar minimo es: " +p2_resultado;
+}
+
+//problema 3
 function problema3(){
     //definir alfabeto
     var alfabeto=['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 
